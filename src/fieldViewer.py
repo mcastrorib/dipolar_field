c1127b73beab5b2223b38adaa1264678057d1b48
matheus <matheusdcribeiro@gmail.com>
2022-12-21 18:58:02 -0300

    dist range updated

--- src/fieldViewer.py
+++ src/fieldViewer.py
@@ -281,8 +281,8 @@ class fieldViewer():
         data = self.field_grads[self.m_map == self.pore_color]
         data = data[data > 1.e-8]
         
-        max_val = np.ceil(np.log10(data.max()+1))
-        min_val = np.floor(np.log10(data.min()-1))
+        max_val = np.ceil(np.log10(data.max()*1.1))
+        min_val = np.floor(np.log10(data.min()*0.9))
         bins = 64
         if(max_val - min_val > 0.0):
             bins *= int(max_val - min_val) 
